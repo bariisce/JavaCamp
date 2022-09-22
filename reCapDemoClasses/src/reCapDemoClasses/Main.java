94490e12b4d9c92e26d42318ca8ec7ffde065279
bariisce <78698824+bariisce@users.noreply.github.com>
2022-09-22 20:31:51 +0300

    java camp's homework

---
+++ reCapDemoClasses/src/reCapDemoClasses/Main.java
@@ -0,0 +1,12 @@
+package reCapDemoClasses;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FourOperations fourOperations = new FourOperations();
+		int result=fourOperations.Division(1200, 60);
+		
+		System.out.println(result);
+	}
+
+}
