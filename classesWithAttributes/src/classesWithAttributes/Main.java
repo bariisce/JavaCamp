e4c9bdd8da17aab849798bf19f2eb038937efd47
bariisce <78698824+bariisce@users.noreply.github.com>
2022-09-22 20:46:02 +0300

    We learned how to use attributes in class

---
+++ classesWithAttributes/src/classesWithAttributes/Main.java
@@ -0,0 +1,19 @@
+package classesWithAttributes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product = new Product();
+		
+		product.id=1;
+		product.name="Laptop";
+		product.description="Lenovo";
+		product.price=7250;
+		product.stockAmount=1;
+
+		
+		ProductManager productManager= new ProductManager();
+		productManager.Add(product);
+	}
+
+}
