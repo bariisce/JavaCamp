66a432c03c1ad2c4dc2b2c4561ad6cac5515dc8f
bariisce <78698824+bariisce@users.noreply.github.com>
2022-09-22 16:44:33 +0300

    learning methods at the camp

--- methods/src/methods/Main.java
+++ methods/src/methods/Main.java
@@ -3,8 +3,31 @@ package methods;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub");
+		String cityName=takeCity();
+		System.out.println(cityName);
+
+		int result=sum(11,8);
+		System.out.println(result);
 
 	}
 
+	public static void add() {
+		System.out.println("Added");
+	}
+	
+	public static void delete() {
+		System.out.println("Deleted");
+	}
+	
+	public static void update() {
+		System.out.println("Updated");
+	}
+	
+	public static int sum(int value1, int value2) {
+		return value1 + value2;
+	}
+	
+	public static String takeCity() {
+		return "Adana";
+	}
 }
