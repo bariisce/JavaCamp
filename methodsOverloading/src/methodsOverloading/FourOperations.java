88ee806a5aa41baa18a32ec3c5417264303b4e0a
bariisce <78698824+bariisce@users.noreply.github.com>
2022-09-23 21:23:04 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ methodsOverloading/src/methodsOverloading/FourOperations.java
@@ -0,0 +1,14 @@
+package methodsOverloading;
+
+public class FourOperations {
+	
+	public int sum(int value1,int value2) {
+		
+		return value1 + value2 ;
+	}
+	
+	public int sum(int value1,int value2,int value3) {
+		
+		return value1 + value2 + value3;
+	}
+}
---
+++ methodsOverloading/src/methodsOverloading/Main.java
@@ -0,0 +1,11 @@
+package methodsOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FourOperations fourOperations = new FourOperations();
+		System.out.println(fourOperations.sum(14, 12));
+		System.out.println(fourOperations.sum(4, 4, 4));
+	}
+
+}
