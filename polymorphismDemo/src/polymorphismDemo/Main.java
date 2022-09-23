22fd1465413cafae062c4136286eb5acc32fef35
bariisce <78698824+bariisce@users.noreply.github.com>
2022-09-23 22:31:25 +0300

    Homework

---
+++ polymorphismDemo/src/polymorphismDemo/Main.java
@@ -0,0 +1,16 @@
+package polymorphismDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//BaseLogger[] loggers = new BaseLogger[] {new DatabaseLogger(),new EmailLogger(),new FileLogger(),new ConsoleLogger() };
+		
+		//for (BaseLogger baseLogger : loggers) {
+			//baseLogger.log("Log Message");
+		//}
+		
+		CustomerManager customerManager = new CustomerManager(new DatabaseLogger());
+		customerManager.add();
+	}
+
+}
